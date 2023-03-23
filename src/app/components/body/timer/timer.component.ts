de4e3acfdf5dae66ce56d92bb810a5028370b463
victor <martinsvictor6781@gmail.com>
2023-03-23 20:36:33 -0300

    refactor timer

--- src/app/components/body/timer/timer.component.ts
+++ src/app/components/body/timer/timer.component.ts
@@ -10,55 +10,58 @@ export class TimerComponent {
   @Input() pomodoro: TimePomodoro = TimePomodoro.POMODORO;
   minutes:number = this.pomodoro;
   seconds:number = 0;
-  percentComplete:number = 0;
   timerInterval:any;
   timerRunning:boolean = false;
 
   @Output() changeStateTimer = new EventEmitter<any>();
 
   ngOnChanges(changes: SimpleChanges) {
-    this.minutes = this.pomodoro;
-    this.seconds = 0;
-    this.percentComplete = 0;
-    if (this.timerInterval){
-      clearInterval(this.timerInterval);
-    }
+    this.resetTimer();
   }
-  startTimer = () => {
-    this.minutes = this.pomodoro
 
-    this.timerInterval = setInterval(() => {
-      if (this.timerRunning){
-        this.decrementSeconds();
-        this.percentComplete = this.getProgressPercent();
-      }
-    }, 1000)
+  getProgressPercent(){
+    return 100 - (100 * (this.minutes * 60 + this.seconds)/(this.pomodoro * 60))
+  }
+
+  restart(){
+    this.resetTimer();
+    this.changeStateTimerAndEmitEvent();
   }
 
   startPause(){
-    this.changeStateTimer.emit()
-    this.timerRunning = !this.timerRunning;
-    if (this.percentComplete === 0 && this.timerRunning){
+    this.changeStateTimerAndEmitEvent();
+    if (this.getProgressPercent() === 0 && this.timerRunning){
       this.startTimer()
     }
   }
 
-  private decrementSeconds(){
+  private startTimer = () => {
+    this.minutes = this.pomodoro
+    this.timerInterval = setInterval(() => {
+      if (this.timerRunning) this.decrementTime();
+    }, 1000)
+  }
+
+  private changeStateTimerAndEmitEvent(){
+    this.changeStateTimer.emit()
+    this.timerRunning = !this.timerRunning;
+  }
+
+  private decrementTime(){
     if (this.seconds === 0){
       if (this.minutes >= 1) this.seconds = 59
-      this.decrementMinutes()
+      if (this.minutes === 0 && this.seconds === 0) clearInterval(this.timerInterval)
+      else this.minutes--
     }else{
       this.seconds--;
     }
   }
 
-  private decrementMinutes(){
-    if (this.minutes === 0 && this.seconds === 0) clearInterval(this.timerInterval)
-    else this.minutes--
-  }
-  private getProgressPercent(){
-    let totalTimeInSeconds = this.pomodoro * 60;
-    let elapsedTimeInSeconds = this.minutes * 60 + this.seconds;
-    return 100 - (100 * elapsedTimeInSeconds/totalTimeInSeconds)
+  private resetTimer(){
+    this.minutes = this.pomodoro;
+    this.seconds = 0;
+    if (this.timerInterval){
+      clearInterval(this.timerInterval);
+    }
   }
 }
